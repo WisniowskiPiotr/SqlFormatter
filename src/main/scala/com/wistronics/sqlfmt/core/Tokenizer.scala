6c5324e1757b76b16622f490ecc063e23fe87dc0
WisniowskiPiotr <contact.wisniowskipiotr@gmail.com>
2020-11-29 12:01:50 +0100

    working tokenizer

--- src/main/scala/com/wistronics/sqlfmt/core/Tokenizer.scala
+++ src/main/scala/com/wistronics/sqlfmt/core/Tokenizer.scala
@@ -1,47 +1,54 @@
 package com.wistronics.sqlfmt.core
 
 import scala.annotation.tailrec
+import scala.util.Try
 
 object Tokenizer {
-  val sqlTokenBorders = Seq(("'", "'", "'"), ("`", "`", "`"), ("--", "\n", "--\n"))
+  val sqlTokenBorders: Seq[(String,String,String)] = Seq(("'", "'", "'"), ("`", "`", "`"), ("--", "\n", "--\n"))
+
+  private def tokenizeCondition(blockExt:String,prefix:String,suffix:String):Boolean={
+    blockExt.startsWith(prefix) && Try(blockExt.substring(prefix.length)).getOrElse("").endsWith(suffix)
+  }
 
   @tailrec
-  private def iterateOverString(output: String, outputTokens: Map[String, String], input: String, block: String)(
+  private def iterateOverString(output: String, outputTokens: Map[String, String], input: Seq[Char], block: String)(
     implicit tokenBorders: Seq[(String, String, String)]
-  ): (String, Map[String, String]) =
-    input.toSeq match {
-      case Seq(c, tail @ _*) if tokenBorders.exists {
-            case (k, _, _) if (block + c).length < k.length => k.startsWith(block + c)
-            case (k, _, _) => (block + c).startsWith(k)
+  ): (String, Map[String, String]) = {
+    input match {
+      case Seq(c, tail @ _*) =>
+        block + c match {
+          // tokenize block
+          case blockExt: String if tokenBorders.exists {
+            case (k, v, _) => tokenizeCondition(blockExt,k,v)
           } =>
-        // key candidate needs next recursion
-        iterateOverString(output = output, outputTokens = outputTokens, input = tail.toString, block = block + c)
-      case Seq(c, tail @ _*) if tokenBorders.exists {
-            case (k, _, _) if block.length < k.length => k.startsWith(block)
-            case (k, _, _) => block.startsWith(k)
+            val tokenBorder = tokenBorders.find { case (k, v, _) => tokenizeCondition(blockExt,k,v) }.map {
+              case (prefix, suffix, replacement) => (prefix.replace("\n", replacement), suffix.replace("\n", replacement))
+            }.get
+            val token = f"${tokenBorder._1}token_${outputTokens.size}${tokenBorder._2}"
+            iterateOverString(
+              output = output + token,
+              outputTokens = outputTokens + (token -> blockExt),
+              input = tail,
+              block = ""
+            )
+          // key candidate needs next recursion
+          case blockExt: String if tokenBorders.exists {
+            case (k, _, _) if blockExt.length < k.length => k.startsWith(blockExt)
+            case (k, _, _) => blockExt.startsWith(k)
           } =>
-        // key candidate invalidated
-        iterateOverString(output = output + block + c, outputTokens = outputTokens, input = tail.toString, block = "")
-      case Seq(c, tail @ _*) if tokenBorders.exists {
-            case (k, v, _) => {
-              val blockExt = block + c
-              blockExt.startsWith(k) && blockExt.endsWith(v)
-            }
-          } =>
-        // tokenize block
-        val blockExt = block + c
-        val tokenBorder = tokenBorders.find { case (k, v, _) => blockExt.startsWith(k) && blockExt.endsWith(v) }.map {
-          case (prefix, suffix, replacement) => (prefix.replace("\n", replacement), suffix.replace("\n", replacement))
-        }.get
-        val token = f"${tokenBorder._1}token_${outputTokens.size}${tokenBorder._2}"
-        iterateOverString(
-          output = output + token,
-          outputTokens = outputTokens + (token -> blockExt),
-          input = tail.toString,
-          block = ""
-        )
-      case _ => (output, outputTokens)
+            iterateOverString(output = output, outputTokens = outputTokens, input = tail, block = blockExt)
+          // no block char
+          case blockExt: String => iterateOverString(
+            output = output + blockExt,
+            outputTokens = outputTokens,
+            input = tail,
+            block = ""
+          )
+        }
+      // end of string
+      case _ => (output + block, outputTokens)
     }
+  }
 
   def excludeNotModifiableStrings(
     input: String
